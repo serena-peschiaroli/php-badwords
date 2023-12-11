9addd3e8ddde8aa9ed49520950dbd938da99cb80
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-12-11 14:38:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Censorship Form</title>
+</head>
+<body>
+    <h1> Invia la tua richiesta </h1>
+
+    <form action="process.php" method="GET">
+        <label for=""> Paragrafo</label>
+        <input type="text" id="paragraph" name="paragraph">
+        <button type="submit"> Invia </button>
+
+    </form>
+    
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Censorship results</title>
+</head>
+<body>
+    <?php 
+
+    //prendi i parametry query
+    $paragraph = $_GET["paragraph"];
+    $wordToCensor = [ 'Lorem' , 'ipsum', 'sed', 'magna'];
+
+    //mostra il paragrafo oroginale e la sua lunghezza
+    //quando il paragrafo è multilinea, si usa nl2br per preservare i line breaks
+
+    echo "<h2>Paragrafo originale:</h2>";
+    echo "<p>" . nl2br($paragraph) . "</p>";
+    echo "<p><strong>Lunghezza:</strong> " . strlen($paragraph) . "<br><br>";
+
+    //parole censurate
+
+    $censoredParagraph = str_ireplace($wordToCensor, '***', $paragraph);
+
+    //mostra il paragrafo censurato e la sua lunghezza
+
+    echo "<h2>Paragrafo Censurato: </h2>";
+    echo nl2br($censoredParagraph) . "<br>";
+    echo "<p><strong>Lunghezza:</strong> "  . strlen($censoredParagraph) . "<br><br>";
+    ?>
+    
+</body>
+</html>
